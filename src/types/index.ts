da2e7a402bd8d87e5bb7b84c1e1c4816be1aefc5
Pablo Espinosa <espipj@gmail.com>
2023-01-09 21:23:04 +0000

    added new types

--- src/types/index.ts
+++ src/types/index.ts
@@ -14,3 +14,32 @@ export interface Bus {
   direction: string;
   time: Date;
 }
+
+export interface StopOSM {
+  type: string;
+  id: number;
+  lat: number;
+  lon: number;
+  tags: Tags;
+}
+
+export interface Tags {
+  bus: string;
+  highway: string;
+  lit?: string;
+  name?: string;
+  operator?: string;
+  public_transport: string;
+  ref?: string;
+  tactile_paving?: string;
+  bench?: string;
+  bin?: string;
+  shelter?: string;
+  mapillary?: string;
+  pole?: string;
+  'survey:date'?: Date;
+  departures_board?: string;
+  start_date?: Date;
+}
+
+export interface GeoPoint extends Pick<StopOSM, 'lat' | 'lon'> {}
